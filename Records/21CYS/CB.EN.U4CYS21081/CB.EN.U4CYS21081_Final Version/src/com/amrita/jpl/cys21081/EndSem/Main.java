2ada3d6e03431dc23fe4a894a93a66ec316787ad
DineshRedddy4512 <114049764+DineshRedddy4512@users.noreply.github.com>
2023-06-25 20:47:11 +0530

    Delete Records/21CYS/CB.EN.U4CYS21081/CB.EN.U4CYS21081_Final Version directory

--- Records/21CYS/CB.EN.U4CYS21081/CB.EN.U4CYS21081_Final Version/src/com/amrita/jpl/cys21081/EndSem/Main.java
+++
@@ -1,8 +0,0 @@
-package com.amrita.jpl.cys21081.EndSem;
-
-public class Main {
-    public static void main(String[] args) {
-        FileManager fileManager = new FileManagerImpl();
-        FileManagementSystemUI fileManagementSystemUI = new FileManagementSystemUI(fileManager);
-    }
-}
